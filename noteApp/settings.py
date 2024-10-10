fd7720f33d065cc2160dd27d98d51e94906d4d5f
awadwehbe <wehbeawad842@gmail.com>
2024-10-10 20:14:02 +0300

    Add Render domain to ALLOWED_HOSTS

--- noteApp/settings.py
+++ noteApp/settings.py
@@ -15,7 +15,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ['noteapp-api-1r.onrender.com']
+# settings.py
+
+ALLOWED_HOSTS = ['noteapp-api-1r.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
